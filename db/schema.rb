60bb73de8a7f5b2e310e9f840c834dc002d6b052
Maurício Medeiros <mauriciomedeiros.05@gmail.com>
2013-11-05 21:26:46 -0200

    Class 5

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131104232556) do
+ActiveRecord::Schema.define(:version => 20131105223229) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "name"
+    t.text     "body"
+    t.integer  "job_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "jobs", :force => true do |t|
     t.string   "title"
